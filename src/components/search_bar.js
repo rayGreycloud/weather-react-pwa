30eda61e67796762aa3346f3493b6d76642c5bb5
rayGreycloud <rayGreycloud@gmail.com>
2017-05-25 17:31:36 -0700

    Add searchbar styles

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
+import '../styles/searchbar.css';
 
 export class SearchBar extends Component {
   constructor(props) {
